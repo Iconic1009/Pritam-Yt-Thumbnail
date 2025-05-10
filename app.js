f3d8d50e647021fa66a7fe961b14410ae91176d2
Iconic1009 <pk22613@gmail.com>
2025-05-10 10:05:22 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import { setupThemeToggle } from './theme.js';
-import { initializeApp } from './main.js';
-
-// Initialize theme first to prevent flash of incorrect theme
-setupThemeToggle();
-
-// Then initialize the rest of the app
-initializeApp();
